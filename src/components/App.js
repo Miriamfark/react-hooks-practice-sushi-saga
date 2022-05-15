1a9dc2039eab78bc25cb0aea13b4e00d8ee381c1
Miriam <miriamjoshua00@gmail.com>
2022-05-15 20:58:50 +0300

    commit tree 5ea31df36da2026951329a845fde3666b8abe36f

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+
+  const [money, setMoney] = useState(50)
+  const [eatenSushi, setEatenSushi] = useState(false)
+
+  const handleEatSushi = (id) => {
+    if(!eatenSushi.includes(id)) {
+      const newEaten = [...eatenSushi, id]
+      setEatenSushi(newEaten)
+    }
+  }
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer eatenSushi={eatenSushi} handleEatSushi={handleEatSushi} />
+      <Table plate={eatenSushi} onMoney={money} onSetMoney={setMoney} />
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi"
 
-function SushiContainer(props) {
+function SushiContainer({ eatenSushi, handleEatSushi }) {
+
+  const [sushis, setSushis] = useState([])
+  const [position, setPosition] = useState(0)
+
+  useEffect(()=>{
+    fetch("http://localhost:3001/sushis")
+  .then((r)=>r.json())
+  .then((data)=>setSushis(data))
+  }, [])
+
+  let sushiArray;
+  if (sushis) {
+    sushiArray = sushis.slice(position, position + 4).map((sushi)=>{
+      return <Sushi 
+      key={sushi.id} 
+      name={sushi.name}
+      image={sushi.img_url}
+      price={sushi.price}
+      eatenSushi={eatenSushi}
+      handleEatSushi={handleEatSushi}
+      />
+    })
+  }
+
+  function handleMoreClick() {
+    setPosition(position + 4)
+  }
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiArray}
+      <MoreButton onHandleMoreClick={handleMoreClick} />
     </div>
   );
 }
